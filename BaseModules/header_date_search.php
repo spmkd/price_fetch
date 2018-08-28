0121a4ba7128398d9c8f44abc51beea63ef922ed
Stole Home <spsonny.sp@gmail.com>
2018-08-28 20:05:15 +0300

    Function Working Properly

--- BaseModules/header_date_search.php
+++ BaseModules/header_date_search.php
@@ -7,18 +7,9 @@ if(!empty($_GET["gameTitle"])){
 
 include 'Functions/createDateLink.php';
 
-
-$last24HoursTo	= date('Y-m-d H:i',time());
-$last24HoursTo	= str_replace(" ", "T", $last24HoursTo);
-$last24HoursFrom= date('Y-m-d H:i', strtotime('-1 Day',time()));
-$last24HoursFrom= str_replace(" ", "T", $last24HoursFrom);
-$last24HoursLink= $_SERVER['PHP_SELF'] . "?fromDate=" . $last24HoursFrom . "&toDate=" . $last24HoursTo . $singleGameIncluded;
-
-$lastWeekTo	= date('Y-m-d H:i',time());
-$lastWeekTo	= str_replace(" ", "T", $lastWeekTo);
-$lastWeekFrom= date('Y-m-d H:i', strtotime('-7 Day',time()));
-$lastWeekFrom= str_replace(" ", "T", $lastWeekFrom);
-$lastWeekLink= $_SERVER['PHP_SELF'] . "?fromDate=" . $lastWeekFrom . "&toDate=" . $lastWeekTo . $singleGameIncluded;
+$last24HoursLink	= CreateDateLink("-1 Day") . $singleGameIncluded;
+$lastWeekLink		= CreateDateLink("-7 Day") . $singleGameIncluded;
+$last30Days 		= CreateDateLink("-30 Day") . $singleGameIncluded;
 
 $fromDate=$_GET["fromDate"];
 
@@ -45,5 +36,6 @@ if(empty($toDate)){
 </form>
 
 <a href="<?php echo $last24HoursLink ?>"> Last 24 Hours </a> &nbsp; &nbsp; &nbsp;
-<a href="<?php echo $lastWeekLink ?>"> Last 7 Days </a>
+<a href="<?php echo $lastWeekLink ?>"> Last 7 Days </a> &nbsp; &nbsp; &nbsp;
+<a href="<?php echo $last30Days ?>"> Last 30 Days </a>
 <br><br>
--- Functions/createDateLink.php
+++ Functions/createDateLink.php
@@ -1,2 +1,15 @@
 <?php
+
+function CreateDateLink($numberOfDays){
+
+	$timeTo	= date('Y-m-d H:i',time());
+	$timeTo	= str_replace(" ", "T", $timeTo);
+	$timeFrom= date('Y-m-d H:i', strtotime($numberOfDays,time()));
+	$timeFrom= str_replace(" ", "T", $timeFrom);
+
+	$varToReturn = $_SERVER['PHP_SELF'] . "?fromDate=" . $timeFrom . "&toDate=" . $timeTo;
+
+	return $varToReturn;
+}
+
 ?>
