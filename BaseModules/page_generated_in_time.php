3be06f89ee3ce833d489e9a2e5b758c4a26b2253
Stole Home <spsonny.sp@gmail.com>
2018-11-04 19:36:37 +0200

    Testing it with GitHub

--- BaseModules/page_generated_in_time.php
+++ BaseModules/page_generated_in_time.php
@@ -6,6 +6,6 @@ $time_taken = $end_time - $start_time;
  
 $time_taken = round($time_taken,5);
  
-echo 'Page generated in '.$time_taken.' seconds!';
+echo 'Page generated in '.$time_taken.' seconds.';
  
 ?>
